3b306425661518c6f30c9378e47f31ec9d3b3a16
Parag Patil <parag6243@gmail.com>
2024-10-04 10:21:36 +0530

    added s3 backend configuration changes

--- provider.tf
+++ provider.tf
@@ -2,4 +2,13 @@ provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
 
+
 # Add your S3 backend configuration here
+
+terraform {
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"
+    key = "Parag.Patil"
+    region = "ap-south-1"
+  }
+}
